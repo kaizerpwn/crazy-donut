3166d4d597e21ee123142360f74b5e7e6fe3af9d
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-02-08 12:09:16 +0100

    allow all origins

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

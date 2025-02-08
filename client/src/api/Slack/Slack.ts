413b7597b33b092c8f4b98f260bc2e18f454424d
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-02-08 12:21:28 +0100

    fix for redirect

--- client/src/api/Slack/Slack.ts
+++ client/src/api/Slack/Slack.ts
@@ -3,11 +3,11 @@ import http from "../http";
 
 export const SlackAPI = {
   getSlackSettings: async (): Promise<SlackSettings> => {
-    const response = await http.get<SlackSettings>("/slack-settings/");
+    const response = await http.get<SlackSettings>("/slack-settings");
     return response.data;
   },
 
   updateSlackSettings: async (settings: SlackSettings): Promise<void> => {
-    await http.put("/slack-settings/", settings);
+    await http.put("/slack-settings", settings);
   },
 };

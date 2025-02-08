0b41945f3999c53ccdf3c2c3d37695a8395b8b95
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-02-08 12:15:04 +0100

    test

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://donut.polodev.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
